bffff1bfb59becbff59354a4d586ad5f5b1ae2f2
Mike Jewett <mikesjewett@gmail.com>
2015-06-18 08:36:03 -0400

    Implement calculations

---
+++ Alcolator/Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Mike Jewett on 6/18/15.
+//  Copyright (c) 2015 Mike Jewett. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
