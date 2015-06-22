14d38a137ed493cac2d403d3d5334b6346206f6c
Mike Jewett <mikesjewett@gmail.com>
2015-06-22 13:07:41 -0400

    Completed Basic Navigation

---
+++ Alcolator/Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Mike Jewett on 6/22/15.
+//  Copyright (c) 2015 Mike Jewett. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MainMenuViewController : ViewController
+
+@end
